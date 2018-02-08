e95734b5b5f0ab5cee22b4d8ce7b6987397c3976
471309445@qq.com <471309445@qq.com>
2018-02-08 09:57:44 +0800

    add content bg

--- js/custom.js
+++ js/custom.js
@@ -682,31 +682,16 @@ function moveline4(){
 	$('.line4').addClass('moveline');
 }
 
-// function draw(){
-// 	$('#cvs-1').css({"height":h,"width":w});
+// function draw(w,h){
+// 	$('#cvs-1').css({"height":h+'px',"width":w+'px'});
 //     var canvas = document.getElementById('cvs-1');
 //     if (canvas.getContext) {
 //         var ctx = canvas.getContext('2d');
-
-//         for (var i = 0; i < 4; i++) {
-//             for (var j = 0; j < 3; j++) {
-//                 ctx.beginPath();
-//                 var x = 25 + j * 50; // x 坐标值
-//                 var y = 25 + i * 50; // y 坐标值
-//                 var radius = 20; // 圆弧半径
-//                 var startAngle = 0; // 开始点
-//                 var endAngle = Math.PI + (Math.PI * j) / 2; // 结束点
-//                 var anticlockwise = i % 2 == 0 ? false : true; // 顺时针或逆时针
-
-//                 ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-
-//                 if (i > 1) {
-//                     ctx.fill();
-//                 } else {
-//                     ctx.stroke();
-//                 }
-//             }
-//         }
+// 		ctx.beginPath();
+// 		ctx.moveTo(0,0);
+// 		ctx.bezierCurveTo(w/2,h/2,w/2,h/2,w,h);
+		
+// 		// ctx.fill();
 //     }
 // }
 
